e28501b771207c2645569384350491942a3d308e
tshulaniHK <25852094@sun.ac.za>
2026-05-15 23:11:04 +0200

    Open day pictures full.

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,12 @@
   }
 
   goTo('home');
+  
+function openLightbox(src) {
+  document.getElementById("lightbox").style.display = "flex";
+  document.getElementById("lightbox-img").src = src;
+}
+
+function closeLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}
